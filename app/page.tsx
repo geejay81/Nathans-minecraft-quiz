faa80acfc49f41a311297bac486b063f1417a116
Greg Parr <greg.parr@blueyonder.co.uk>
2024-03-02 22:04:09 +0000

    added some hover and focus styling to form controls

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   
   return (
       <div className="flex flex-col w-screen px-5 h-screen bg-[#1A1A1A] justify-center items-center">
-        <h1 className="text-white font-bold text-3xl">Nathan&apos;s Minecraft Quiz</h1>
         <Quiz />
       </div>
   );
--- components/quiz.tsx
+++ components/quiz.tsx
@@ -55,7 +55,7 @@ export default function Quiz() {
                 </h1>
             ): (
                 <React.Fragment>
-                
+                <h1 className="text-white font-bold text-3xl">Nathan&apos;s Minecraft Quiz</h1>
                 <div className="flex flex-col items-start w-full">
                     <h2 className="mt-10 text-xl text-white/70">
                         Question {currentQuestion + 1} of {questions.length}
@@ -68,7 +68,8 @@ export default function Quiz() {
                 {questions[currentQuestion].answerOptions.map((answer, index) => (
                     <div
                     key={index}
-                    className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5"
+                    className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5
+                        hover:border-white focus-within:border-white"
                     onClick={(e) => handleAnswerOption(answer.answer)}
                     >
                     <input
@@ -89,13 +90,13 @@ export default function Quiz() {
                 <div className="flex justify-between w-full mt-4 text-white">
                     <button
                         onClick={handlePrevious}
-                        className="w-[49%] py-3 bg-green-600 rounded-lg"
+                        className="w-[49%] py-3 bg-green-600 rounded-lg border-2 border-green-600 hover:border-white focus:border-white"
                         >Previous</button>
                     <button
                         onClick={
                             currentQuestion + 1 === questions.length ? handleSubmitButton : handleNext
                         }
-                        className="w-[49%] py-3 bg-green-600 rounded-lg"
+                        className="w-[49%] py-3 bg-green-600 rounded-lg border-2 border-green-600 hover:border-white focus:border-white"
                         >
                         {currentQuestion + 1 === questions.length ? "Submit" : "Next"}
                         </button>
