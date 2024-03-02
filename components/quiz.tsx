a1519d1e2e949c4827e4c876221944810a48db7d
Greg Parr <greg.parr@blueyonder.co.uk>
2024-03-02 21:16:58 +0000

    added remaining questions

--- components/quiz.tsx
+++ components/quiz.tsx
@@ -13,6 +13,8 @@ export default function Quiz() {
 
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [selectedOptions, setSelectedOptions] = useState(initialSelectedOptions);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(false);
 
     const handlePrevious = () => {
         const prevQues = currentQuestion - 1;
@@ -31,49 +33,78 @@ export default function Quiz() {
         setSelectedOptions([...selectedOptions]);
     };
 
-    return (
+    const handleSubmitButton = () => {
+        let newScore = 0;
+        for (let i = 0; i < questions.length; i++) {
+          questions[i].answerOptions.map(
+            (answer) =>
+              answer.isCorrect &&
+              answer.answer === selectedOptions[i]?.answerByUser &&
+              (newScore += 1)
+          );
+        }
+        setScore(newScore);
+        setShowScore(true);
+      };
+
+      return (
         <React.Fragment>
-            <div className="flex flex-col items-start w-full">
-                <h2 className="mt-10 text-xl text-white/70">
-                    Question {currentQuestion + 1} of {questions.length}
-                </h2>
-                <div className="mt-4 text-2xl text-white">
-                    {questions[currentQuestion].question}
+            {showScore ? (
+                <h1 className="text-3xl font-semibold text-center text-white">
+                    You scored {score} out of {questions.length}
+                </h1>
+            ): (
+                <React.Fragment>
+                
+                <div className="flex flex-col items-start w-full">
+                    <h2 className="mt-10 text-xl text-white/70">
+                        Question {currentQuestion + 1} of {questions.length}
+                    </h2>
+                    <div className="mt-4 text-2xl text-white">
+                        {questions[currentQuestion].question}
+                    </div>
                 </div>
-            </div>
-            <div className="flex flex-col w-full">
-            {questions[currentQuestion].answerOptions.map((answer, index) => (
-                <div
-                key={index}
-                className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5"
-                onClick={(e) => handleAnswerOption(answer.answer)}
-                >
-                <input
-                    type="radio"
-                    name={answer.answer}
-                    value={answer.answer}
-                    onChange={(e) => handleAnswerOption(answer.answer)}
-                    checked={
-                    answer.answer === selectedOptions[currentQuestion]?.answerByUser
-                    }
-                    className="w-6 h-6 bg-black"
-                />
-                <p className="ml-6 text-white">{answer.answer}</p>
+                <div className="flex flex-col w-full">
+                {questions[currentQuestion].answerOptions.map((answer, index) => (
+                    <div
+                    key={index}
+                    className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer border-white/10 rounded-xl bg-white/5"
+                    onClick={(e) => handleAnswerOption(answer.answer)}
+                    >
+                    <input
+                        type="radio"
+                        name={answer.answer}
+                        value={answer.answer}
+                        onChange={(e) => handleAnswerOption(answer.answer)}
+                        checked={
+                        answer.answer === selectedOptions[currentQuestion]?.answerByUser
+                        }
+                        className="w-6 h-6 bg-black"
+                    />
+                    <p className="ml-6 text-white">{answer.answer}</p>
+                    </div>
+                ))
+                }
                 </div>
-            ))
-            }
-            </div>
-            <div className="flex justify-between w-full mt-4 text-white">
-                <button
-                    onClick={handlePrevious}
-                    className="w-[49%] py-3 bg-green-600 rounded-lg"
-                    >Previous</button>
-                <button
-                    onClick={handleNext}
-                    className="w-[49%] py-3 bg-green-600 rounded-lg"
-                    >Next</button>
-            </div>
+                <div className="flex justify-between w-full mt-4 text-white">
+                    <button
+                        onClick={handlePrevious}
+                        className="w-[49%] py-3 bg-green-600 rounded-lg"
+                        >Previous</button>
+                    <button
+                        onClick={
+                            currentQuestion + 1 === questions.length ? handleSubmitButton : handleNext
+                        }
+                        className="w-[49%] py-3 bg-green-600 rounded-lg"
+                        >
+                        {currentQuestion + 1 === questions.length ? "Submit" : "Next"}
+                        </button>
+    
+                </div>
+                
+            </React.Fragment>
+            )}
         </React.Fragment>
-    )
+      )
     
 }
